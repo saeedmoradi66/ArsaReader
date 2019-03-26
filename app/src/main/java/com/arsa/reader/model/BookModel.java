2736ce5caeb96a205eeac67314f3afec7f121fd6
سعید مرادی <saeed.moradi3@gmail.com>
2019-03-27 00:39:52 +0430

    init

---
+++ app/src/main/java/com/arsa/reader/model/BookModel.java
@@ -0,0 +1,58 @@
+package com.arsa.reader.model;
+
+public class BookModel {
+    private int bookID;
+    private String bookTitle;
+    private String author;
+    private String translator;
+    private String description;
+    private String content;
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTranslator() {
+        return translator;
+    }
+
+    public void setTranslator(String translator) {
+        this.translator = translator;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/arsa/reader/model/PackageModel.java
@@ -0,0 +1,49 @@
+package com.arsa.reader.model;
+
+public class PackageModel {
+    private int packageID;
+    private int categoryID;
+    private String packageTitle;
+    private int price;
+    private boolean isActive;
+
+    public int getPackageID() {
+        return packageID;
+    }
+
+    public void setPackageID(int packageID) {
+        this.packageID = packageID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getPackageTitle() {
+        return packageTitle;
+    }
+
+    public void setPackageTitle(String packageTitle) {
+        this.packageTitle = packageTitle;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
