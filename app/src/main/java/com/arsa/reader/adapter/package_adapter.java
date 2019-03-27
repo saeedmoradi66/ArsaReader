b4e4b2acbcf4c321440b85863150870dea8039c2
aliasqar <aliasqarrahmani@gmail.com>
2019-03-27 23:25:07 +0430

    add ratingBar 98/1/7

--- app/src/main/java/com/arsa/reader/adapter/package_adapter.java
+++ app/src/main/java/com/arsa/reader/adapter/package_adapter.java
@@ -2,11 +2,13 @@ package com.arsa.reader.adapter;
 
 import android.app.Activity;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.androidnetworking.interfaces.ParsedRequestListener;
@@ -32,6 +34,7 @@ public class package_adapter extends ArrayAdapter<PackageModel> {
 
         TextView titleText = (TextView) rowView.findViewById(R.id.tv_title);
         TextView priceText = (TextView) rowView.findViewById(R.id.tv_price);
+        RatingBar ratingBar = (RatingBar) rowView.findViewById(R.id.ratingBar);
         final com.androidnetworking.widget.ANImageView package_image = rowView.findViewById(R.id.package_image);
 
         final String URL_IMAGE = "http://testclub.ir/showmenupic.ashx?id=25";
@@ -44,7 +47,7 @@ public class package_adapter extends ArrayAdapter<PackageModel> {
         Typeface pricetypeface = Typeface.createFromAsset(context.getAssets(), "fonts/Vazir-Light-FD.otf");
         priceText.setTypeface(pricetypeface);
         priceText.setText(_Data.get(position).Price+" ریال ");
-
+        ratingBar.setRating(_Data.get(position).Rate);
         return rowView;
 
     };
