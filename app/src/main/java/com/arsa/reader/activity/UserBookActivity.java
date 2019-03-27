c7b0b9ca44822a2dd3d03054df4b64b90ce61573
سعید مرادی <saeed.moradi3@gmail.com>
2019-03-27 16:43:29 +0430

    980107-saeed

---
+++ app/src/main/java/com/arsa/reader/activity/UserBookActivity.java
@@ -0,0 +1,122 @@
+package com.arsa.reader.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.ParsedRequestListener;
+import com.arsa.reader.R;
+import com.arsa.reader.adapter.package_adapter;
+import com.arsa.reader.model.PackageModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class UserBookActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_book);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+
+        List_fill(1);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserBookActivity.this, UserPackageActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void List_fill(int id) {
+        final Activity context = this;
+        AndroidNetworking.get(getString(R.string.server_url) + "/Package/GetByUserID/{id}")
+                .addPathParameter("id", String.valueOf(id))
+                .setTag(this)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsObjectList(PackageModel.class, new ParsedRequestListener<List<PackageModel>>() {
+                    @Override
+                    public void onResponse(List<PackageModel> packagelist) {
+
+                        package_adapter adapter = new package_adapter(context, packagelist);
+
+                        ListView list = findViewById(R.id.lv_package);
+                        if (list != null)
+                            list.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+
+                    }
+
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
