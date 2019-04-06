34cb284a1896408bce3f28b5a5091a8e9cd72797
aliasqar <aliasqarrahmani@gmail.com>
2019-04-06 10:49:32 +0430

    navigation header 98/1/9

---
+++ app/src/main/java/com/arsa/reader/tab/tab_Books.java
@@ -0,0 +1,17 @@
+package com.arsa.reader.tab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arsa.reader.R;
+
+import androidx.fragment.app.Fragment;
+
+public class tab_Books extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_books, container, false);
+    }
+}
