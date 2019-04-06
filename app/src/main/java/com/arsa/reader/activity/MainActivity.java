b8cae463537ddb783d83ba8fc234486c9cd19871
aliasqar <aliasqarrahmani@gmail.com>
2019-04-06 10:46:44 +0430

    navigation header 98/1/9

--- app/src/main/java/com/arsa/reader/activity/MainActivity.java
+++ app/src/main/java/com/arsa/reader/activity/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends BaseActivity {
             super.onBackPressed();
         }
     }
-   /* @Override
+   /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.cart, menu);
